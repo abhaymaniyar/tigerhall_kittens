2618e813fb6d7fce44b2130ffe0c8c23f9fc01ec
Abhay Maniyar <abhay.maniyar@getsimpl.com>
2024-03-15 02:41:09 +0530

    feat: add router in main with tiger endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
 package cmd
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"tigerhall_kittens/internal/handler"
+)
+
+func main() {
+	router := httprouter.New()
+
+	tigerHandler := handler.NewTigerHandler()
+	router.POST("/api/v1/tigers", tigerHandler.CreateTiger())
+	router.GET("/api/v1/tigers", tigerHandler.ListTigers())
+
+	http.ListenAndServe(":8080", router)
+}
