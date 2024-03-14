1f21d536739714345e8a95aa919f391bfebc9e24
Abhay Maniyar <abhay.maniyar@getsimpl.com>
2024-03-15 01:35:58 +0530

    feat: added deps

---
+++ internal/db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"tigerhall_kittens/internal/model"
+)
+
+func ConnectAndMigrate() *gorm.DB {
+	// TODO: use env variables instead of hard coded strings
+	dsn := "host=localhost user=abhay dbname=tigerhall_kittens sslmode=disable password=rootroot"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to db: %v", err)
+	}
+
+	// Run migrations
+	// TODO: use a better db migration tool, ex: goose
+	db.AutoMigrate(&model.User{}, &model.Tiger{}, &model.Sighting{})
+
+	return db
+}
